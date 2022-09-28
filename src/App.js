611fc91ccfd73fe76eb67c81ce86a54d0ed1b134
vaibhav  sharma <80534369+iamvaibhav31@users.noreply.github.com>
2022-09-28 18:31:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/index'
-import Navbar from './components/Navbar';
-import Fotter from './components/fotter';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend'
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="bg-gradient d-flex flex-column vh-100 clearflex">
-        {/* <Navbar /> */}
+        
         <Routes>
           <Route exact path='Dot-Product-Solutions/' element={<Home />} />
         </Routes>
